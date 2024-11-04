a15e9d287836e08d2261d7abeaabd0bfb94c98e1
GinoGallardo <ginosgt@gmail.com>
2024-11-04 00:08:59 -0300

    update code

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors:{
+      Grey_700: "hsl(0, 0%, 20%)",
+      Grey_800: "hsl(0, 0%, 12%)",
+      Grey_900: "hsl(0, 0%, 8%)",
+      Green: "hsl(75, 94%, 57%)",
+      textoPrimary: "#ffffff",
+      textoSecundary: "#fcfcfc",
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
